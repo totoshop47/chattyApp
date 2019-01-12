0da0fc63ba370f6c71eaa428d3bb07bc716b2929
Jimyung Park <pzy51992@hotmail.com>
2019-01-12 01:12:39 +0000

    split indes.html by different components in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,73 @@
 import React, {Component} from 'react';
 
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    }
+  }
+
   render() {
+    // console.log(this.state.currentUser.name)
     return (
-      <h1>Hello React :)</h1>
-    );
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+        <ChatBar />
+      </div>
+    )
   }
 }
+
+class ChatBar extends Component {
+  render(){
+    return(
+      <footer className="chatbar">
+        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      </footer>
+    )
+  }
+}
+
+// class Messages extends Component {
+//   render(){
+//     return(
+
+//     )
+//   }
+// }
+
+class MessageList extends Component {
+  render(){
+    return(
+      <main className="messages">
+        <div className="message">
+          <span className="message-username">Anonymous1</span>
+          <span className="message-content">I won't be impressed with technology until I can download food.</span>
+        </div>
+        <div className="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>
+      </main>
+    )
+  }
+}
+
+
 export default App;
