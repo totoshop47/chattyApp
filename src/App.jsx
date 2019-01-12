6a8016899d33f07204a2e22436516fdc490db462
Jimyung Park <pzy51992@hotmail.com>
2019-01-12 04:36:46 +0000

    modify app.jsx to better way

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import React, {Component} from 'react';
 
 class App extends Component {
   constructor(props){
-    super(props);
+    super(props)
 
     this.state = {
       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: [
         {
+          id: 1,
           username: "Bob",
           content: "Has anyone seen my marbles?",
         },
         {
+          id: 2,
           username: "Anonymous",
           content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
         }
@@ -20,15 +22,30 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
+
   render() {
-    // console.log(this.state.currentUser.name)
+
     return (
       <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <MessageList />
-        <ChatBar />
+        <main className="messages">
+            <MessageList messages={this.state.messages} />
+        </main>
+        <ChatBar user={this.state.currentUser}/>
       </div>
     )
   }
@@ -36,35 +53,52 @@ class App extends Component {
 
 class ChatBar extends Component {
   render(){
+    const currentUser = this.props.user;
+
     return(
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={currentUser.name} />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     )
   }
 }
 
-// class Messages extends Component {
-//   render(){
-//     return(
-
-//     )
-//   }
-// }
-
 class MessageList extends Component {
+  constructor(props){
+    super(props)
+
+  }
+
   render(){
+    const messagesFromApp = this.props.messages;
+    const msgsArray = messagesFromApp.map( (msg, index) => {
+      return <Message key={index} message={msg.content} username={msg.username}/>
+     })
+
     return(
       <main className="messages">
-        <div className="message">
-          <span className="message-username">Anonymous1</span>
-          <span className="message-content">I won't be impressed with technology until I can download food.</span>
-        </div>
+        <h1> underone </h1>
+        { msgsArray }
         <div className="message system">
           Anonymous1 changed their name to nomnom.
         </div>
       </main>
+    )
+  }
+}
+
+class Message extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render(){
+    return(
+      <div className="message">
+        <span className="message-username">{this.props.username}</span>
+        <span className="message-content">{this.props.message}</span>
+      </div>
     )
   }
 }
